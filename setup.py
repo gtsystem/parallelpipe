b5f8dfe29f8ca58b5febf2769a310e97e7f01771
Giuseppe Tribulato <gtsystem@users.noreply.github.com>
2016-06-19 13:44:23 +0200

    Adding documentation and tox.ini

--- setup.py
+++ setup.py
@@ -1,14 +1,23 @@
 from setuptools import setup
 
+try:
+    import re
+    import pypandoc
+    long_description = pypandoc.convert('README.md', 'rst')
+    long_description = re.sub("[.][.] figure.*?:alt:", "", long_description, flags=re.M+re.S)
+except Exception as e:
+    # pandoc or pypandoc is not installed, fallback to using raw contents
+    with open('README.md') as f:
+        long_description = f.read()
 
 setup(
     name='parallelpipe',
-    version='0.1',
+    version='0.2.4',
     author='Giuseppe Tribulato',
     author_email='gtsystem@gmail.com',
     py_modules=['parallelpipe'],
     url='https://github.com/gtsystem/parallelpipe',
-    license='See LICENSE.txt',
+    license='MIT',
     description='Pipeline parallelization library',
-    long_description=open('README.md').read(),
+    long_description=long_description,
 )
